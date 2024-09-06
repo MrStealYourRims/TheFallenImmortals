5a80b0d41af82c3b4118b612527dc1bbb9e1b352
MrStealYourRims <86498461+MrStealYourRims@users.noreply.github.com>
2024-09-06 12:17:00 -0400

    Update ajax.js
    updated the ajax and javascript

--- ajax.js
+++ ajax.js
@@ -1,53 +1,54 @@
 // JavaScript Document
-
 //////////////////
 //AJAX Functions//
 //////////////////
-function evalAJAXHtml(source){
-	if(window.XMLHttpRequest){
-		var ajax = new XMLHttpRequest();
-	}
-	else if(window.ActiveXObject){
-		var ajax = new ActiveXObject("Microsoft.XMLHTTP");
-	}
 
-	ajax.open("GET",source,true);
-	ajax.onreadystatechange = function(){
-		try{
-			if(ajax.readyState == 4){
-				if(ajax.status == 200){
-					eval(ajax.responseText);
-				}
-			}
-		}
-		catch(e){
-			//Exception-bug in FF
-		}
-	}
-	ajax.send(null);
+// Helper function to create an XMLHttpRequest object
+function createXHR() {
+    return new XMLHttpRequest();
 }
 
-function evalpostAJAXHtml(source,datan){
-	if(window.XMLHttpRequest){
-		var ajax = new XMLHttpRequest();
-	}
-	else if(window.ActiveXObject){
-		var ajax = new ActiveXObject("Microsoft.XMLHTTP");
-	}
-
-	ajax.open("POST",source,true);
-	ajax.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-	ajax.onreadystatechange = function(){
-		try{
-			if(ajax.readyState == 4){
-				if (ajax.status == 200){
-					eval(ajax.responseText);
-				}
-			}
-		}
-		catch(e){
-			//Exception-bug in FF
-		}
-	}
-	ajax.send(datan);
+// Function to evaluate AJAX response (GET method)
+function evalAJAXHtml(source) {
+    const xhr = createXHR();
+    xhr.open("GET", source, true);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                try {
+                    eval(xhr.responseText);
+                } catch (e) {
+                    console.error("Error evaluating AJAX response:", e);
+                }
+            } else {
+                console.error("AJAX request failed. Status:", xhr.status);
+            }
+        }
+    };
+    xhr.send();
 }
+
+// Function to evaluate AJAX response (POST method)
+function evalPostAJAXHtml(source, data) {
+    const xhr = createXHR();
+    xhr.open("POST", source, true);
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                try {
+                    eval(xhr.responseText);
+                } catch (e) {
+                    console.error("Error evaluating AJAX response:", e);
+                }
+            } else {
+                console.error("AJAX request failed. Status:", xhr.status);
+            }
+        }
+    };
+    xhr.send(data);
+}
+
+// Example usage of the functions
+// evalAJAXHtml('https://api.example.com/data');
+// evalPostAJAXHtml('https://api.example.com/update', 'key1=value1&key2=value2');
