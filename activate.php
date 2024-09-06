ffcb938a4f9f676715f63b695f267f70e323a641
MrStealYourRims <86498461+MrStealYourRims@users.noreply.github.com>
2024-09-06 11:59:55 -0400

    Update activate.php to php 8.3

--- activate.php
+++ activate.php
@@ -1,36 +1,52 @@
 <?php
-include('db.php');
+require_once 'db.php';
+
+function activateAccount(PDO $pdo, string $key): ?array {
+    $stmt = $pdo->prepare("SELECT * FROM activation WHERE `key` = :key");
+    $stmt->execute(['key' => $key]);
+    return $stmt->fetch(PDO::FETCH_ASSOC);
+}
+
+function deleteActivationKey(PDO $pdo, string $key): void {
+    $stmt = $pdo->prepare("DELETE FROM activation WHERE `key` = :key");
+    $stmt->execute(['key' => $key]);
+}
+
+function activateCharacter(PDO $pdo, string $username): void {
+    $stmt = $pdo->prepare("UPDATE characters SET activated = 'Yes' WHERE username = :username");
+    $stmt->execute(['username' => $username]);
+}
+
+$message = "Cannot find Activation Key. Please follow the link sent to your email address.";
+
+if (isset($_GET['key']) && !empty($_GET['key'])) {
+    try {
+        $pdo = new PDO("mysql:host=localhost;dbname=your_database_name", "username", "password");
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        $key = activateAccount($pdo, $_GET['key']);
+
+        if ($key) {
+            $message = "The account, {$key['username']} has now been activated. You may now start playing immediately.<br /><a href='index.php'>Login Here</a>";
+            deleteActivationKey($pdo, $_GET['key']);
+            activateCharacter($pdo, $key['username']);
+        } else {
+            $message = "This Activation Key cannot be found. Please follow the link sent to your email address.";
+        }
+    } catch (PDOException $e) {
+        $message = "An error occurred: " . $e->getMessage();
+    }
+}
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html lang="en">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=windows-1250" />
-<title>Rise Of Immortals ~ Activation</title>
-<link href="main.css" rel="stylesheet" type="text/css" />
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rise Of Immortals ~ Activation</title>
+    <link href="main.css" rel="stylesheet" type="text/css">
 </head>
-
 <body>
-<?php
-if($_GET['key'] != "")
-{
-	$findkey = mysql_query("SELECT * FROM activation WHERE `key`='".$_GET['key']."'") or die(mysql_error());
-	if(mysql_num_rows($findkey) == "1")
-	{
-		$key = mysql_fetch_assoc($findkey);
-		echo("<center>The account, ".$key['username']." has now been activated. You may now start playing immediately.<br /><a href='index.php'>Login Here</a></center>");
-
-		$deletekey = mysql_query("DELETE FROM activation WHERE `key`='".$_GET['key']."'") or die(mysql_error());
-		$activatechar = mysql_query("UPDATE characters SET activated='Yes' WHERE username='".$key['username']."'");
-	}
-	else
-	{
-		echo("<center>This Activation Key cannot be found. Please follow the link sent to your email address.</center>");
-	}
-}
-else
-{
-	echo("<center>Cannot find Activation Key. Please follow the link sent to your email address.</center>");
-}
-?>
+    <center><?= htmlspecialchars($message) ?></center>
 </body>
 </html>
